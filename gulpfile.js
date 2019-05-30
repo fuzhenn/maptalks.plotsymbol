0271edd2a23d319bff07b4f8fbb8d3447e39a387
fuzhenn <fuzhen@maptalks.org>
2019-05-30 11:08:51 +0800

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,28 @@ const bundleHelper = new BundleHelper(pkg);
 const testHelper = new TestHelper();
 const karmaConfig = require('./karma.config');
 
-gulp.task('build', () => {
-    return bundleHelper.bundle('index.js');
+gulp.task('build', done => {
+    bundleHelper.bundle('index.js');
+    done();
 });
 
-gulp.task('minify', gulp.series(['build'], () => {
+gulp.task('minify', gulp.series(['build'], done => {
     bundleHelper.minify();
+    done();
 }));
 
 gulp.task('watch', gulp.series(['build'], () => {
-    gulp.watch(['index.js', 'src/**/*', './gulpfile.js'], ['build']);
+    gulp.watch(['index.js', 'src/**/*', './gulpfile.js'], gulp.series(['build']));
 }));
 
-gulp.task('test', gulp.series(['build'], () => {
+gulp.task('test', gulp.series(['build'], done => {
     testHelper.test(karmaConfig);
+    done();
 }));
 
 gulp.task('tdd', gulp.series(['build'], () => {
     karmaConfig.singleRun = false;
-    gulp.watch(['index.js', 'src/**/*'], ['test']);
+    gulp.watch(['index.js', 'src/**/*'], gulp.series(['test']));
     testHelper.test(karmaConfig);
 }));
 
