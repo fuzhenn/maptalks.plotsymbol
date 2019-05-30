c62cf363adec84c27a8c57c89f6ac61036a8baf4
fuzhenn <fuzhen@maptalks.org>
2019-05-30 11:05:01 +0800

    upgrade to gulp 4.x

--- gulpfile.js
+++ gulpfile.js
@@ -10,23 +10,23 @@ gulp.task('build', () => {
     return bundleHelper.bundle('index.js');
 });
 
-gulp.task('minify', ['build'], () => {
+gulp.task('minify', gulp.series(['build'], () => {
     bundleHelper.minify();
-});
+}));
 
-gulp.task('watch', ['build'], () => {
+gulp.task('watch', gulp.series(['build'], () => {
     gulp.watch(['index.js', 'src/**/*', './gulpfile.js'], ['build']);
-});
+}));
 
-gulp.task('test', ['build'], () => {
+gulp.task('test', gulp.series(['build'], () => {
     testHelper.test(karmaConfig);
-});
+}));
 
-gulp.task('tdd', ['build'], () => {
+gulp.task('tdd', gulp.series(['build'], () => {
     karmaConfig.singleRun = false;
     gulp.watch(['index.js', 'src/**/*'], ['test']);
     testHelper.test(karmaConfig);
-});
+}));
 
-gulp.task('default', ['watch']);
+gulp.task('default', gulp.series(['watch']));
 
